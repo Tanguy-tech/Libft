7f9da53535108afaa44f67f186c407b7ed7e7977
Tanguy Billon <tanguy_billon@hotmail.fr>
2021-07-27 09:42:25 +0200

    add ft_putchar && norme ok

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tanguy <tanguy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 14:29:30 by tbillon           #+#    #+#             */
-/*   Updated: 2021/07/27 09:36:38 by tanguy           ###   ########.fr       */
+/*   Updated: 2021/07/27 09:39:58 by tanguy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,6 +56,7 @@ char		*ft_strtrim(char const *s1, char const *set);
 char		**ft_split(char const *s, char c);
 char		*ft_itoa(int n);
 char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void    	ft_putchar(char c);
 void		ft_putchar_fd(char c, int fd);
 void		ft_putstr_fd(char *s, int fd);
 void		ft_putendl_fd(char *s, int fd);
